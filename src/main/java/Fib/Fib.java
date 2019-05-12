613d29b5d5a79f7448f08c31bc0981fde42c0634
Bartosz Dabrowski/Janosik <bart.dabrowski.uk@gmail.com>
2019-05-12 10:54:13 +0100

    fib nie rekurencyjnie

--- src/main/java/Fib/Fib.java
+++ src/main/java/Fib/Fib.java
@@ -1,5 +1,7 @@
 package Fib;
 
+import java.util.Scanner;
+
 public class Fib {
 
     public static int fibonacci(int n)
@@ -14,9 +16,38 @@ public class Fib {
         }
     }
 
+ public static void Fib()
+ {
+     Scanner s = new Scanner(System.in);
+     int n = s.nextInt();
 
+     if(n<2)
+     {
+         System.out.println(n);
+     }
+     else{
+         int a =0;
+         System.out.println(a);
+         int b = 1;
+         System.out.println(b);
+         int c = 1;
+         System.out.println(c);
+         int k =2;
+         while(k<n)
+         {
+             a = b;
+             b=c;
+             System.out.println(c= a+b);
+             k++;
+
+         }
+             //System.out.println(c);
+     }
+ }
     public static void main(String[] args) {
-        System.out.println(fibonacci(7));
+        //System.out.println(fibonacci(7));
+        System.out.println("Podaj liczbe z jakiego zakresu chcesz wyznaczyc ciag Fibonnaciego");
+        Fib();
 
     }
 
